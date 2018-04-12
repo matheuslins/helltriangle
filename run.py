3b831c70714454b2f1605446c3526b25d150ac42
Matheus Lins <msl@cin.ufpe.br>
2018-04-12 20:45:27 -0300

    add run.py file to call main module

---
+++ run.py
@@ -0,0 +1,9 @@
+from modules.triangle import HellTriangle
+from utils import raw_input_triangle_size as size_t
+
+    
+hell_triangle = HellTriangle()
+hell_triangle.summed_triangle([[6], [3, 5], [9, 7, 1], [4, 6, 8, 4]])
+print ("Running randomly...")
+hell_triangle(size_t())
+
